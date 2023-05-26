44a53905ef69084de348fdaabd33753b2d853281
Imran Chowdhury <imran.cuet.cse17@gmail.com>
2023-05-27 02:34:42 +0600

    added token interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
+import { HomeComponent } from './home/home.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { PostTitleComponent } from './shared/post-title/post-title.component';
+import { SubredditSideBarComponent } from './shared/subreddit-side-bar/subreddit-side-bar.component';
+import { VoteButtonComponent } from './shared/vote-button/vote-button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TockenInterceptorService } from './tocken-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +23,11 @@ import { ToastrModule } from 'ngx-toastr';
     HeaderComponent,
     LoginComponent,
     SignUpComponent,
+    HomeComponent,
+    SideBarComponent,
+    PostTitleComponent,
+    SubredditSideBarComponent,
+    VoteButtonComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,8 +35,15 @@ import { ToastrModule } from 'ngx-toastr';
     ReactiveFormsModule,
     HttpClientModule,
     ToastrModule.forRoot(),
+    FontAwesomeModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TockenInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
